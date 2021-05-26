536b74f83a1c9f0f146335e926a788bfab1098d8
Narwade <sushank3100@gmail.com>
2021-05-26 13:09:28 +0530

    Deploy my PostJob APIs and mongoDB cloud data

--- server.js
+++ server.js
@@ -13,13 +13,11 @@ app.get('/', (req, res) => res.send('API is Live and Running Perfectly'));
 
 //Define Routes
 app.use('/api/auth', require('./routes/api/auth'));
-
-app.use('/home', require('./routes/api/postjob'));
 app.use('/api/postjob', require('./routes/api/postjob'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/recruiters', require('./routes/api/recruiters'));
 app.use('/api/jobstatus', require('./routes/api/jobstatus'));
-
+app.use('/home', require('./routes/api/postjob'));
 const PORT = process.env.PORT || 1010;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
